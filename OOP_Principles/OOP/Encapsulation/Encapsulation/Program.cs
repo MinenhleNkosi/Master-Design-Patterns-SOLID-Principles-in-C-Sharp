52bf03a46d703d267b4f20df2f0e167bf0645c78
Minenhle <minenclenkosi@gmail.com>
2025-03-29 12:35:38 +0200

    New changes

--- OOP_Principles/OOP/Encapsulation/Encapsulation/Program.cs
+++ OOP_Principles/OOP/Encapsulation/Encapsulation/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Encapsulation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BadExample badExampleBalance = new BadExample();
+        badExampleBalance.balance = 100;
+        Console.WriteLine(badExampleBalance.balance);
+        Console.ReadLine();
+    }
+}
