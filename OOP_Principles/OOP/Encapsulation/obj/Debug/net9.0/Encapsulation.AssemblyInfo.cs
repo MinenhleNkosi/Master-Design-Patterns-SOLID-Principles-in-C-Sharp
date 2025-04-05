e38aca349dd81a515a287d9be805f20acbb828f5
Minenhle <minenclenkosi@gmail.com>
2025-04-05 08:04:19 +0200

    New changes

--- OOP_Principles/OOP/Encapsulation/obj/Debug/net9.0/Encapsulation.AssemblyInfo.cs
+++ OOP_Principles/OOP/Encapsulation/obj/Debug/net9.0/Encapsulation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Encapsulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+873be23bb7ada419e82df24f6ae293b5d367aa76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755bffddd8788738310de48b2aaa4d996c4be729")]
 [assembly: System.Reflection.AssemblyProductAttribute("Encapsulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Encapsulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
